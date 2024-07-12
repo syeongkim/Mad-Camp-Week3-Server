bc7e1353aa9a10882d2ff9fc864a6db29589f14b
seoyeong <carsk0607@gmail.com>
2024-07-12 20:04:09 +0900

    connect express.js and mongodb

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 // server.js
 const express = require('express');
+const mongoose = require('mongoose');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+const PORT = process.env.PORT;
 const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+const MONGO_URI = process.env.MONGO_URI;
 const REDIRECT_URI = 'http://localhost:3000/oauth/github/callback';
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
 
 app.get('/oauth/github', (req, res) => {
   const url = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=read:user`;
@@ -48,6 +60,6 @@ app.get('/oauth/github/callback', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
